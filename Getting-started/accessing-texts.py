73c2bf0788f97bd614b31fa38c7091630f9b527b
Ashwin Rai <raiashwin018@gmail.com>
2017-08-16 19:26:54 +0530

    additional resources

---
+++ Getting-started/accessing-texts.py
@@ -0,0 +1,14 @@
+from nltk.book import *
+
+#Access texts by calling out their names
+#print(text1)
+
+# texts() function
+#print(texts())
+
+# sents() function
+#print(sents(), end='\n\n\n')
+
+# access sentences individually
+#print(sent1)
+
